2d8b0201a647e52b5708832f54f1e1c9d8277db9
manoj190802 <manoj.manojlenin@gmail.com>
2026-06-14 15:05:22 +0530

    initial13

--- student-score.js
+++ student-score.js
@@ -1,36 +1,49 @@
-const students=[]
-function addstudent(name,score){
-    score=Number(score)
-    score=score ?? 0
-    name=name.trim().toUpperCase();
-    students.push({name,score})
-}
-// grade
-function getgrade(){
-   return students.map((students)=>{
-        let grade="f"
-        if(students.score>=90) grade="A"
-        else if(students.score>=75) grade="B"
-        else if (students.score>=65) grade="C"
-        else if (students.score>=35) grade="D"
-        return {...students,grade}
+// const students=[]
+// function addstudent(name,score){
+//     score=Number(score)
+//     score=score ?? 0
+//     name=name.trim().toUpperCase();
+//     students.push({name,score})
+// }
+// // grade
+// function getgrade(){
+//    return students.map((students)=>{
+//         let grade="f"
+//         if(students.score>=90) grade="A"
+//         else if(students.score>=75) grade="B"
+//         else if (students.score>=65) grade="C"
+//         else if (students.score>=35) grade="D"
+//         return {...students,grade}
         
-    })
-}
+//     })
+// }
 
-// top score
-function getTopScore(){
-    return students.filter((students)=>students.score>=90)
+// // top score
+// function getTopScore(){
+//     return students.filter((students)=>students.score>=90)
 
-}
-// /search
-function search(name){
-    return students.find((students)=>students.name==name)
+// }
+// // /search
+// function search(name){
+//     return students.find((students)=>students.name==name)
+    
+// }
+// // check failure
+// function cheackfailures(){
+//    return students.some((students)=>students.score<35)
+   
+
+// }
+
+
+
+// Functiomal programming
+const person={
+    name:"john",
+    age:30,
     
 }
-// check failure
-function cheackfailures(){
-   return students.some((students)=>students.score<35)
-   document.write("there is a failure")
-
+function getperson(){
+    console.log(person.name,person.age,person)
 }
+getperson()
